393a42838c2620a2ebd0c0efd105fa3b24c58c70
Dennisgarcia5 <148032530+Dennisgarcia5@users.noreply.github.com>
2023-10-15 23:19:32 -0600

    Add files via upload

---
+++ sema11/sema11/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using (var contextoDB = new contextoDB())
+{
+    foreach (var item in contextoDB.estudiantes)
+    {
+        Console.WriteLine($"Datos: Id: {item.Id}, Nombres: {item.Nombres}, Apellidos: {item.Apellidos}, Sexo: {item.Sexo}, Edad: {item.Edad}");
+
+    }
+}
+
+bool agregarMasRegistros = true;
+
+while (agregarMasRegistros)
+{
+    Console.WriteLine("Ingrese los datos del estudiante:");
+
+    Console.Write("Nombres: ");
+    string nombres = Console.ReadLine();
+
+    Console.Write("Apellidos: ");
+    string apellidos = Console.ReadLine();
+
+    Console.Write("Sexo: ");
+    string sexo = Console.ReadLine();
+
+    Console.Write("Edad: ");
+    int edad = Convert.ToInt32(Console.ReadLine());
+
+    using (var contextoDB = new contextoDB())
+    {
+        contextoDB.Database.EnsureCreated();
+
+        var nuevoEstudiante = new estudianteUNAB()
+        {
+            Nombres = nombres,
+            Apellidos = apellidos,
+            Sexo = sexo,
+            Edad = edad
+        };
+
+        contextoDB.Add(nuevoEstudiante);
+        contextoDB.SaveChanges();
+    }
+
+    Console.WriteLine("¿Desea agregar más registros? (Sí/No)");
+    string respuesta = Console.ReadLine();
+    agregarMasRegistros = respuesta.StartsWith("S", StringComparison.OrdinalIgnoreCase);
+}
+
+using (var contextoDB = new contextoDB())
+{
+    foreach (var estudiante in contextoDB.estudiantes)
+    {
+        Console.WriteLine($"Estudiante: {estudiante.Nombres} {estudiante.Apellidos}, Sexo: {estudiante.Sexo}, Edad: {estudiante.Edad}");
+    }
+}
